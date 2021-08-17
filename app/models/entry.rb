9e426fcfcde679f78fa54043b0a132fe0cc16e95
taylor-fuller <taylor.fuller@tutanota.com>
2021-08-16 18:04:54 -0700

    commit on main

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,4 +2,8 @@ class Entry < ApplicationRecord
     validates :calories, :proteins, :carbohydrates, :fats, numericality: { only_integer: true,
         message: "Please enter a valid number" }
     validates_presence_of :meal_type, message: 'Please select an option'
+
+    def day
+        self.created_at.strftime("%B %e, %Y")
+    end
 end
