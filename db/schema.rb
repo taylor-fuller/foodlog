da8123af6385fdab054a9559b0f11e0db39ae92c
taylor-fuller <thfuller92@gmail.com>
2021-08-21 18:47:22 -0700

    commit on main

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_20_084325) do
+ActiveRecord::Schema.define(version: 2021_08_22_014354) do
 
   create_table "entries", force: :cascade do |t|
     t.string "meal_type"
@@ -29,6 +29,18 @@ ActiveRecord::Schema.define(version: 2021_08_20_084325) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["entry_id"], name: "index_foods_on_entry_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "foods", "entries"
