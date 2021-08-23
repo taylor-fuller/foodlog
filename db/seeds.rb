8187dd9b395f3d9a37a7beb0b93c88334b967dc9
taylor-fuller <thfuller92@gmail.com>
2021-08-23 11:40:04 -0700

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'thfuller92@gmail.com', password: 'c493|pR?ZG%I', password_confirmation: 'c493|pR?ZG%I') if Rails.env.development?
